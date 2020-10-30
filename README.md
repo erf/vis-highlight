aaffe5341aedd07f45b279d2a5f94db2e3bd5420
Erlend Lind Madsen <erlendf80@gmail.com>
2020-10-30 15:35:51 +0100

    set custom style

--- README.md
+++ README.md
@@ -2,14 +2,22 @@
 
 A [vis-plugin](https://github.com/martanne/vis/wiki/Plugins/) to highlight patterns.
 
-## How to
+## Commands
  
-`:hi [pattern] (on/off)` - highlight text with given Lua pattern
+`:hi [pattern] (on/off)` - highlight text with Lua pattern
 
 `:hi-ls` - list patterns
 
 `:hi-cl` - clear patterns
 
-## Patterns
+See [Patterns](https://www.lua.org/pil/20.2.html)
 
-See [Lua docs](https://www.lua.org/pil/20.2.html)
+## Style
+
+You can style the matches with:
+
+```
+require('plugins/vis-highlight').style = 'back:#444444,fore:#cccccc'
+```
+
+See Style definition in [LPegLexer](https://scintilla.sourceforge.io/LPegLexer.html)
--- init.lua
+++ init.lua
@@ -1,7 +1,10 @@
 local M = {}
 
 M.patterns = {}
-M.style = "" -- TOOD
+
+-- TODO set for a spesific pattern
+M.STYLE_ID = 0
+M.style = nil
 
 -- highlight patterns in visible area
 function highlight(pattern, win)
@@ -18,7 +21,12 @@ function highlight(pattern, win)
 
 		local style_start  = from - 1 + viewport.start
 		local style_finish = ends - 1 + viewport.start
-		win:style(win.STYLE_CURSOR, style_start, style_finish)
+
+		if M.style then
+			win:style(M.STYLE_ID, style_start, style_finish)
+		else
+			win:style(win.STYLE_CURSOR, style_start, style_finish)
+		end
 
 		init = ends + 1
 	end
@@ -33,6 +41,14 @@ function on_win_highlight(win)
 end
 
 vis.events.subscribe(vis.events.WIN_HIGHLIGHT, on_win_highlight)
+
+function on_win_open(win)
+	if M.style then
+		win:style_define(M.STYLE_ID, M.style)
+	end
+end
+
+vis.events.subscribe(vis.events.WIN_OPEN, on_win_open)
 
 function hi_command(argv, force, win, selection, range)
 	local pattern_arg = argv[1]
